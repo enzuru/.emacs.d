6984ea51d3cc83519f35ad269915fdca483c63fb
Ahmed Khanzada <me@enzu.ru>
2020-02-15 13:17:47 -0800

    Setup sunrise submodule

--- enzuru/enzuru-file-manager.el
+++ enzuru/enzuru-file-manager.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/local/sunrise-commander")
 
-(require 'sunrise-commander)
-(require 'sunrise-x-modeline)
-(require 'sunrise-x-tree)
+(require 'sunrise)
+(require 'sunrise-modeline)
+(require 'sunrise-tree)
 
 (add-to-list 'auto-mode-alist '("\\.srvm\\'" . sr-virtual-mode))
