b66b6831681a25b4e415bddd60c9636075c27ece
Ahmed Khanzada <lenzuru@gmail.com>
2020-04-19 17:23:08 -0700

    Setting up CEDET

--- enzuru/modes/enzuru-c.el
+++ enzuru/modes/enzuru-c.el
@@ -1,3 +1,5 @@
+(require 'semantic/sb)
+
 (defun enzuru-c-mode-hook ()
   (setq c-default-style "bsd"
         c-basic-offset 4
@@ -5,3 +7,6 @@
         indent-tabs-mode t))
 
 (add-hook 'c-mode-hook 'enzuru-c-mode-hook)
+
+(global-ede-mode 1)
+(semantic-mode 1)
