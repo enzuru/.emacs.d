82b69d496d3612df9cf4ebceec8715575bcf261c
Ahmed Khanzada <lenzuru@gmail.com>
2020-04-21 01:07:39 -0700

    More Objective-C improvements

--- enzuru/modes/enzuru-objective-c.el
+++ enzuru/modes/enzuru-objective-c.el
@@ -1,1 +1,25 @@
 (add-hook 'objc-mode-hook #'lsp)
+
+(defun buffer-contains-substring (string)
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (search-forward string nil t))))
+
+(defun cocoa-open-documentation ()
+  (interactive)
+  (let ((foundation-url (concat "https://developer.apple.com/documentation/foundation/" (downcase (thing-at-point 'word))))
+        (appkit-url (concat "https://developer.apple.com/documentation/appkit/" (downcase (thing-at-point 'word)))))
+    (let ((previous-buffer (current-buffer))
+          (web-result (url-retrieve-synchronously foundation-url)))
+      (switch-to-buffer web-result)
+      (if (buffer-contains-substring "404 Not Found")
+          (browse-url appkit-url)
+        (browse-url foundation-url))
+      (switch-to-buffer previous-buffer))))
+
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-x C-d") 'cocoa-open-documentation)
+            (local-set-key (kbd "C-x C-r") 'lsp-ui-peek-find-references)
+            (local-set-key (kbd "C-x C-w") 'lsp-ui-peek-find-definitions)))
--- enzuru/operating-systems/enzuru-linux.el
+++ enzuru/operating-systems/enzuru-linux.el
@@ -1,5 +1,6 @@
 (setq locate-command "find")
 (setq ccls-executable "/var/lib/snapd/snap/bin/ccls")
+(setq browse-url-browser-function 'browse-url-firefox)
 
 (defun async-term ()
   "Open terminal"
