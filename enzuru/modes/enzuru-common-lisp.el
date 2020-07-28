5bf632b7c1a0ac25fb0a5055d49da69bb2dc1c70
Ahmed Khanzada <me@enzu.ru>
2020-07-27 21:54:27 -0700

    Switch buffer on sly eval

--- enzuru/modes/enzuru-common-lisp.el
+++ enzuru/modes/enzuru-common-lisp.el
@@ -1,8 +1,13 @@
+(defun sly-eval-buffer-and-switch ()
+  (interactive)
+  (sly-eval-buffer)
+  (switch-to-buffer-other-window "*sly-mrepl for sbcl*"))
+
 (add-hook 'lisp-mode-hook
           (lambda ()
             (local-set-key (kbd "C-x C-d") 'sly-describe-function)
             (local-set-key (kbd "C-x C-w") 'sly-edit-definition-other-window)
-            (local-set-key (kbd "C-x c") 'sly-eval-buffer)))
+            (local-set-key (kbd "C-x c") 'sly-eval-buffer-and-switch)))
 
 (add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (lispy-mode 1)))
