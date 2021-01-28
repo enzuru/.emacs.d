b4718ca74a6aa4d422b9783f184132ce574d1654
Ahmed Khanzada <enzuru@ultra.enzu.ru>
2021-01-27 22:50:44 -0800

    Fix Common Lisp documentation

--- enzuru/modes/enzuru-common-lisp.el
+++ enzuru/modes/enzuru-common-lisp.el
@@ -1,6 +1,6 @@
 (add-hook 'lisp-mode-hook
           (lambda ()
-            (local-set-key (kbd "C-x C-d") 'sly-describe-function)
+            (local-set-key (kbd "C-x C-d") 'sly-documentation)
             (local-set-key (kbd "C-x C-w") 'sly-edit-definition-other-window)
             (local-set-key (kbd "C-x c") 'sly-eval-buffer)))
 
