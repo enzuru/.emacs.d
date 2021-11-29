9f38588c9b6a906017a0e69d95f843afd4a90bce
Ahmed Khanzada <ahmed_khanzada@mckinsey.com>
2021-11-29 13:54:59 -0500

    Cleanup enzuru-linux.el

--- enzuru/operating-systems/enzuru-linux.el
+++ enzuru/operating-systems/enzuru-linux.el
@@ -1,9 +1,25 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
-(if window-system
-    (require 'enzuru-window-management))
+;; Functions
 
-(require 'enzuru-guix)
+(defun enzuru-setup-desktop ()
+  (require 'enzuru-window-management)
+  (require 'enzuru-guix)
+  (find-file "~/notes/index.org")
+  (split-window-right)
+  (switch-to-buffer "index.org")
+  (with-eval-after-load 'geiser-guile
+    (add-to-list 'geiser-guile-load-path "~/src/guix")))
+
+(defun enzuru-async-term ()
+  "Open terminal"
+  (interactive)
+  (start-process "urxvtc" "urxvtc" "urxvtc" "-fn" "xft:Iosevka:style=Regular:size=15"))
+
+(if window-system
+    (enzuru-setup-desktop))
+
+;; Variables
 
 (setq locate-command "locate -d /home/enzuru/locatedb.n")
 (setq ccls-executable "ccls")
@@ -11,18 +27,8 @@
 (setq inferior-lisp-program "sbcl")
 (setq multi-term-program "fish")
 
-(defun enzuru-async-term ()
-  "Open terminal"
-  (interactive)
-  (start-process "urxvtc" "urxvtc" "urxvtc" "-fn" "xft:Iosevka:style=Regular:size=15"))
+;; Keys
 
 (global-set-key (kbd "C-c x") 'enzuru-async-term)
-
-(with-eval-after-load 'geiser-guile
-  (add-to-list 'geiser-guile-load-path "~/src/guix"))
-
-(find-file "~/notes/index.org")
-(split-window-right)
-(switch-to-buffer "index.org")
 
 (provide 'enzuru-linux)
--- enzuru/preferences/enzuru-packages.el
+++ enzuru/preferences/enzuru-packages.el
@@ -3,13 +3,10 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("gnu" .
-               "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" .
-               "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
+(package-refresh-contents)
 
 (package-install 'use-package)
 
