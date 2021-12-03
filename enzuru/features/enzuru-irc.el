b1dbefb8b5a705b5150ab16dd3fe3f7a97d78258
Ahmed Khanzada <me@enzu.ru>
2021-12-02 22:12:23 -0500

    Less IRC channels

--- enzuru/features/enzuru-irc.el
+++ enzuru/features/enzuru-irc.el
@@ -7,17 +7,11 @@
         erc-user-full-name "Ahmed Khanzada")
 
   (setq erc-autojoin-channels-alist
-        '(("libera.chat" "#emacs" "#guix" "#openbsd" "#lisp" "#guile" "#gnu")
-          ("oftc.net" "#cat-v")
-          ("sorcery.net" "#square")
-          ("psychz.net" "#c-64")))
+        '(("libera.chat" "#emacs" "#guix" "#guile" "#gnu")))
 
   (defun enzuru-load-irc ()
     (interactive)
-    (erc-tls :server "irc.libera.chat" :port "6697" :password "password")
-    (erc-tls :server "irc.oftc.net" :port "6697" :password "password")
-    (erc-tls :server "themis.sorcery.net" :port "6697" :password "password")
-    (erc-tls :server "psychz.ssl.ircnet.ovh" :port "6697" :password "password")))
+    (erc-tls :server "irc.libera.chat" :port "6697" :password "password")))
 
 ;; Packages
 
