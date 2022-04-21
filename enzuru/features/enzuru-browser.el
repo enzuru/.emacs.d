205b9dc95c94fbe044f570a2abefb9237ee26ec7
Ahmed Khanzada <me@enzu.ru>
2022-04-20 21:53:12 -0400

    Browser fix

--- enzuru/features/enzuru-browser.el
+++ enzuru/features/enzuru-browser.el
@@ -4,7 +4,8 @@
   (defun enzuru-search ()
     (interactive)
     (eww-browse-url (concat "https://search.marginalia.nu/search?profile=yolo&js=no-js&query=" (read-string "Search: "))))
-  (setq browse-url-browser-function 'eww-browse-url))
+  (setq browse-url-browser-function 'eww-browse-url)
+  (global-set-key (kbd "") 'enzuru-search))
 
 ;; Packages
 
