2f938035cac3ac7b673feb7167f3a6bed881e27d
Ahmed Khanzada <me@enzu.ru>
2022-07-02 14:31:30 -0400

    Add spreadsheet functionality

---
+++ enzuru/features/enzuru-spreadsheet.el
@@ -0,0 +1,14 @@
+;; -*- coding: utf-8; lexical-binding: t -*-
+
+;; Configuration
+
+(defun enzuru-configure-dismal ()
+  (interactive))
+
+;; Packages
+
+(use-package dismal
+  :ensure t
+  :config (enzuru-configure-dismal))
+
+(provide 'enzuru-spreadsheet)
