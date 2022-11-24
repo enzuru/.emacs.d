a4bea992598103c7403371cffc4efe4c8a7fb6fc
Ahmed Khanzada <me@enzu.ru>
2022-11-24 14:05:06 -0500

    Colorful Sly REPL

--- enzuru/modes/enzuru-clojure.el
+++ enzuru/modes/enzuru-clojure.el
@@ -3,7 +3,9 @@
 ;; Packages
 
 (use-package cider
-  :ensure t)
+  :ensure t
+  :bind (:map clojure-mode-map
+              ("C-x c" . cider-eval-buffer)))
 
 (use-package clojure-mode
   :ensure t)
--- enzuru/modes/enzuru-common-lisp.el
+++ enzuru/modes/enzuru-common-lisp.el
@@ -1,9 +1,19 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
+;; Directories
+
+(add-to-list 'load-path "~/.emacs.d/local/sly-repl-ansi-color")
+
+;; Functions
+
+(defun enzuru-configure-sly ()
+  (push 'sly-repl-ansi-color sly-contribs))
+
 ;; Packages
 
 (use-package sly
   :ensure t
+  :config (enzuru-configure-sly)
   :bind (:map lisp-mode-map
               ("C-x C-d" . sly-documentation)
               ("C-x C-w" . sly-edit-definition-other-window)
