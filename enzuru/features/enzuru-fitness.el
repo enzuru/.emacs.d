38920ddf84b13bfb71191b599e5a6ca6b1293679
Ahmed Khanzada <me@enzu.ru>
2023-06-12 23:49:13 -0400

    Improved RMR

--- enzuru/features/enzuru-fitness.el
+++ enzuru/features/enzuru-fitness.el
@@ -1,8 +1,9 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
 (defun enzuru-fitness-monthly-deficit (daily-calories daily-training-hours)
-  (let ((rmr 2000)
+  (let ((rmr 2150)
         (burned-per-hour 400))
-    (* 30 (- rmr (- daily-calories (* daily-training-hours burned-per-hour))))))
+    (/ (* 30 (- rmr (- daily-calories (* daily-training-hours burned-per-hour))))
+       3500)))
 
 (provide 'enzuru-fitness)
