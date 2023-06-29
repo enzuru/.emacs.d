e5c02dc27d7450144ccd6c0e375c514c84ae77b5
Ahmed Khanzada <ahmed_khanzada@mckinsey.com>
2023-06-29 15:10:23 -0400

    More accurate RMR

--- enzuru/features/enzuru-fitness.el
+++ enzuru/features/enzuru-fitness.el
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
 (defun enzuru-fitness-monthly-deficit (daily-calories daily-training-hours)
-  (let ((rmr 2150)
+  (let ((rmr 2175)
         (burned-per-hour 400))
     (/ (* 30 (- rmr (- daily-calories (* daily-training-hours burned-per-hour))))
        3500)))
