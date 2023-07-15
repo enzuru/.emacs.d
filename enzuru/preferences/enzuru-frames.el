fc3f742df02e4bb8db46f4eba39fea0d8662b32d
Ahmed Khanzada <me@enzu.ru>
2023-07-15 17:09:09 -0400

    Better name

--- enzuru/preferences/enzuru-frames.el
+++ enzuru/preferences/enzuru-frames.el
@@ -14,7 +14,7 @@
   (global-set-key (kbd "C-z n") 'tab-bar-switch-to-next-tab)
   (global-set-key (kbd "C-z p") 'tab-bar-switch-to-next-tab)
   (global-set-key (kbd "C-z k") 'tab-bar-close-tab)
-  (global-set-key (kbd "C-x C-n") 'counsel-describe-tab))
+  (global-set-key (kbd "C-x C-n") 'counsel-tabs-describe-tab))
 
 (defun enzuru-configure-winner-mode ()
   (winner-mode 1))
