43784f996167294e6e1055721457d405434527e2
Ahmed Khanzada <me@enzu.ru>
2023-07-15 18:33:42 -0400

    Better Scheme bindings

--- enzuru/modes/enzuru-scheme.el
+++ enzuru/modes/enzuru-scheme.el
@@ -3,23 +3,15 @@
 ;; Configuration
 
 (defun enzuru-configure-geiser-guile ()
-  (require 'geiser)
   (require 'geiser-guile)
   (add-hook 'scheme-mode-hook 'geiser-mode)
-  (define-key geiser-mode-map (kbd "C-x c") 'geiser-load-current-buffer)
-  (define-key geiser-mode-map (kbd "C-x C-d") 'geiser-doc-symbol-at-point))
+  (define-key scheme-mode-map (kbd "C-x c") 'geiser-load-current-buffer)
+  (define-key scheme-mode-map (kbd "C-x C-d") 'geiser-doc-symbol-at-point))
 
-(defun enzuru-configure-ivy-guile ()
-  (define-key geiser-mode-map (kbd "C-h s") 'ivy-geiser-describe-symbol))
+(defun enzuru-configure-ivy-guile ()  
+  (define-key scheme-mode-map (kbd "C-h s") 'ivy-geiser-describe-symbol))
 
 ;; Packages
-
-(straight-use-package
- '(geiser
-   :protocol https
-   :type git
-   :host gitlab
-   :repo "emacs-geiser/geiser"))
 
 (straight-use-package
  '(geiser-guile
