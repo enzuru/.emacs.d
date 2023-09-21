fbf953675758d7a553830fc2f8fe6cd3ce9078bc
Ahmed Khanzada <me@enzu.ru>
2023-09-20 21:42:27 -0400

    Switched over to custom deep-thought-theme

--- enzuru/themes/enzuru-deep-thought.el
+++ enzuru/themes/enzuru-deep-thought.el
@@ -4,20 +4,17 @@
 
 (defun enzuru-configure-deep-thought-theme ()
   (load-theme 'deep-thought t)
-  (if (eq 'global-hl-line-mode 1) (set-face-background 'linum "#000F14"))
-  (set-face-attribute 'fringe nil :background "#000F14" :foreground "#FFFFFF")
-  (set-face-foreground 'mode-line-inactive "#888888")
-  (set-face-background 'mode-line-inactive "#000F14")
-  (set-face-background 'mode-line "#113450")
-  (set-face-attribute 'vertical-border nil :foreground "#000F14")
   (sml/apply-theme 'respectful)
+  (set-face-attribute 'fringe nil :background "#000F14" :foreground "#FFFFFF")
+  (set-face-attribute 'vertical-border nil :foreground "#000F14")
   (custom-set-faces
-   '(mode-line ((t (:background "#001520" :foreground "#FFFFFF" :inverse-video nil :box nil))))
-   '(mode-line-inactive ((t (:inherit mode-line :background "#001520" :foreground "gray40" :inverse-video nil :box nil :weight light))))))
+   '(mode-line ((t (:background "#001520" :foreground "#FFFFFF" ))))
+   '(mode-line-inactive ((t (:inherit mode-line :background "#001520" :foreground "gray40")))))
+  )
 
 ;; Packages
 
-(straight-use-package '(deep-thought-theme :type git :host github :repo "emacsfodder/emacs-deep-thought-theme"))
+(straight-use-package '(deep-thought-theme :type git :host github :repo "enzuru/emacs-deep-thought-theme"))
 (enzuru-configure-deep-thought-theme)
 
 (provide 'enzuru-deep-thought)
