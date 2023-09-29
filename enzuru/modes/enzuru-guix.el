6c3056cb0467e198914f158cc64e0f5c2171f8fc
Ahmed Khanzada <me@enzu.ru>
2023-09-29 17:08:59 -0400

    Upgrade zpb-ttf

--- enzuru/modes/enzuru-guix.el
+++ enzuru/modes/enzuru-guix.el
@@ -14,8 +14,11 @@
   (async-shell-command "~/bin/guix-upgrade" "*guix-upgrade*")
   (async-shell-command "cd ~/quicklisp/local-projects/stumpwm && git pull" "*stumpwm-upgrade*")
   (async-shell-command "cd ~/quicklisp/local-projects/clx-truetype && git pull" "*truetype-upgrade*")
+  (async-shell-command "cd ~/quicklisp/local-projects/zpb-ttf && git pull" "*zpb-ttf-upgrade*")
   (async-shell-command "cd ~/.stumpwm.d/modules && git pull" "*stumpwm-modules-upgrade*")
-  (enzuru-setup-tab "upgrading" "*stumpwm-upgrade*" "*guix-upgrade*" "*truetype-upgrade*" "*Messages*" "*stumpwm-modules-upgrade*"))
+  (enzuru-setup-tab "upgrading"
+                    "*stumpwm-upgrade*" "*truetype-upgrade*" "*guix-upgrade*"  "*Messages*"
+                    "*stumpwm-modules-upgrade*" "*zpb-ttf-upgrade*"))
 
 (defun enzuru-reboot ()
   (shell-command "sudo reboot"))
