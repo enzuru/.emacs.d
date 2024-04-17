ae0f8aa14915e7088395b21274575bab838569d5
Ahmed Khanzada <merboob.khanzada@fisglobal.com>
2024-04-17 17:13:19 -0400

    swap OpenBSD and FreeBSD themes

--- enzuru/features/enzuru-gui.el
+++ enzuru/features/enzuru-gui.el
@@ -13,9 +13,9 @@
   (if (eq system-type 'windows-nt)
       (require 'enzuru-arjen-grey))
   (if (eq (enzuru-uname) 'FreeBSD)
-      (require 'enzuru-omtose-darker))
-  (if (eq (enzuru-uname) 'OpenBSD)
       (require 'enzuru-zenburn))
+  (if (eq (enzuru-uname) 'OpenBSD)
+      (require 'enzuru-omtose-darker))
   (if (eq system-type 'gnu/linux)
       (require 'enzuru-deep-thought)))
 
