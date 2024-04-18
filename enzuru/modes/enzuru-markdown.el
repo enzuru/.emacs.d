65415b1e3430abbf9d02635008afe11050ca1360
Ahmed Khanzada <ahmed_khanzada@mckinsey.com>
2024-04-18 10:11:26 -0400

    Improve org and markdown mode

--- enzuru/modes/enzuru-markdown.el
+++ enzuru/modes/enzuru-markdown.el
@@ -2,7 +2,7 @@
 
 ;; Configuration
 
-(defun enzuru-hook-markdown ()
+(defun enzuru-hook-markdown-mode ()
   (setq-local completion-at-point-functions
               (mapcar #'cape-company-to-capf (list #'company-spell))))
 
@@ -10,7 +10,7 @@
 
 (use-package markdown-mode
   :ensure t
-  :hook ((markdown-mode . enzuru-hook-markdown))
+  :hook ((markdown-mode . enzuru-hook-markdown-mode))
   :mode (("\\.md$" . markdown-mode)
          ("\\.gmi$" . markdown-mode)))
 
--- enzuru/modes/enzuru-org.el
+++ enzuru/modes/enzuru-org.el
@@ -8,12 +8,17 @@
         org-src-fontify-natively t
         org-startup-folded nil))
 
+(defun enzuru-hook-org-mode ()
+  (setq-local completion-at-point-functions
+              (mapcar #'cape-company-to-capf (list #'company-spell))))
+
 ;; Packages
 
 (use-package org
   :ensure t
   :defer t
-  :hook ((org-mode . hyperbole-mode))
+  :hook ((org-mode . hyperbole-mode)
+         (org-mode . enzuru-hook-org-mode))
   :bind (:map org-mode-map
               ("C-c b" . org-insert-heading))
   :config (enzuru-configure-org))
