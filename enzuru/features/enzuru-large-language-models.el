985ed27a98669ab7a0fd4e3e890273d0b44d7d56
Ahmed Khanzada <me@enzu.ru>
2025-07-14 23:53:36 -0400

    Change gptel to use Claude

--- enzuru/features/enzuru-large-language-models.el
+++ enzuru/features/enzuru-large-language-models.el
@@ -21,19 +21,24 @@
 (defun enzuru-configure-claude-code ()
   (claude-code-mode))
 
-;; Packages
+(defun enzuru-configure-gptel ()
+  (setq gptel-model 'claude-3-7-sonnet-20250219)
+  (setq gptel-backend (gptel-make-anthropic "Claude" :stream t :key 'gptel-api-key-from-auth-source)))
 
-(use-package gptel
-  :ensure t
-  :defer t
-  :bind (:map gptel-mode-map
-              ("C-c o" . gptel-send)))
+;; Packages
 
 (use-package claude-code
   :ensure (:type git :host github :repo "enzuru/claude-code.el" :branch "update-transient")
   :defer t
   :bind-keymap ("C-c c" . claude-code-command-map) ;; or your preferred key
   :config (enzuru-configure-claude-code))
+
+(use-package gptel
+  :ensure t
+  :defer t
+  :bind (:map gptel-mode-map
+              ("C-c o" . gptel-send))
+  :config (enzuru-configure-gptel))
 
 (provide 'enzuru-large-language-models)
 
