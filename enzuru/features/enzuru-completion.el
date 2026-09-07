c3f299cb8c772bc740e6ded4b0601cf3eb3aa94f
Elias Khanzada <enzuru@thundermail.com>
2026-09-06 17:15:14 -0700

    fix old consult names

--- enzuru/features/enzuru-completion.el
+++ enzuru/features/enzuru-completion.el
@@ -117,8 +117,8 @@
   (consult-customize consult-theme :preview-key '(:debounce 0.2 any)
                      consult-ripgrep consult-git-grep consult-grep
                      consult-bookmark consult-recent-file consult-xref
-                     consult--source-bookmark consult--source-file-register
-                     consult--source-recent-file consult--source-project-recent-file
+                     consult-source-bookmark consult-source-file-register
+                     consult-source-recent-file consult-source-project-recent-file
                      :preview-key '(:debounce 0.4 any)))
 
 (use-package consult-ag
