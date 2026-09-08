4f56269f56c2be6600af29457d9ef11df6bced02
Elias Khanzada <enzuru@thundermail.com>
2026-09-07 20:44:28 -0700

    Remove Efrit entirely

--- enzuru/features/enzuru-large-language-models.el
+++ enzuru/features/enzuru-large-language-models.el
@@ -140,16 +140,6 @@ Interactively, prompt for a known project."
   :bind (("C-c A" . enzuru-agent-shell-new-shell))
   :config (enzuru-configure-agent-shell))
 
-(defun enzuru-configure-efrit ()
-  (let ((default-directory "~/.emacs.d/elpaca/repos/efrit/lisp"))
-    (add-to-list 'load-path default-directory)
-    (normal-top-level-add-subdirs-to-load-path))
-  (require 'efrit))
-
-;; (use-package efrit
-;;   :ensure (:type git :host github :repo "riwatt/efrit")
-;;   :config (enzuru-configure-efrit))
-
 (provide 'enzuru-large-language-models)
 
 ;;; enzuru-large-language-models.el ends here
